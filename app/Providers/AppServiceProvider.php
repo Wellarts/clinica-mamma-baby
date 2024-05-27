56b3d06a5dfe9678d6fd748d3a01643b6ac794ed
Wellington Cavalcante <wellarts@gmail.com>
2024-05-26 22:05:06 -0300

    updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Number;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-       
+
     }
 
     /**
@@ -20,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Number::useLocale('pt_BR');
     }
 }
